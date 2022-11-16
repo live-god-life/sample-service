66ed4444dde56121b1c688a0c65df11619662fc1
fDevJc <fdevjc@gmail.com>
2022-11-16 19:59:45 +0900

    젠킨스 파일 테스트

--- src/main/java/com/godlife/sampleservice/domain/Sample.java
+++ src/main/java/com/godlife/sampleservice/domain/Sample.java
@@ -6,6 +6,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "samples")
 public class Sample {
